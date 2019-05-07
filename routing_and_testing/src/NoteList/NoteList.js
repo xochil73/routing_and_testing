180149c1293fdeed3dd79cd5a2bf9300699af2c9
Xochil Squaglia <xochilsquaglia@yahoo.com>
2019-05-06 20:51:18 -0700

    pushing up

--- routing_and_testing/src/NoteList/NoteList.js
+++ routing_and_testing/src/NoteList/NoteList.js
@@ -10,7 +10,7 @@ export default class NoteList extends React.Component {
           this.props.notes.map(currentNote => {
             return <li key={currentNote.id}>
               {currentNote.title} : {currentNote.content}
-              <button onClick={this.props.removeNote.bind(this, currentNote.id)}>Delete Note</button>
+              <button onClick={this.props.removeNote.bind(this.currentNote.id)}>Delete Note</button>
             </li>
         })
         }
